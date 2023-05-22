bfe3b6f880e021a98d5fb71a425ac9656f7f878e
hieupt <phamhieu078@gmail.com>
2023-05-22 08:01:50 +0700

    update

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,109 +1,30 @@
 
+#include <Arduino.h>
 #include <WiFi.h>
-// Bluetooth LE
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEScan.h>
-#include <BLEAdvertisedDevice.h>
+#include <HTTPClient.h>
+#define MQTT_MAX_PACKET_SIZE 1844
+#include <PubSubClient.h>
 
-#include "time.h"
+const char *ssid = "ESP8266";
+const char *password = "0123456789";
 
-// const char *ssid = "VCCorp";
-// const char *password = "Vcc123**";
+String serverName = "http://192.168.4.1/";
 
-const char *ssid = "mmdholdings801";
-const char *password = "mmdholdings801";
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+// unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
 
-const char *ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 3600;
-const int daylightOffset_sec = 3600;
+/* MQTT credentials and connection */
+const char *mqttServer = "192.168.4.3";
+const int mqttPort = 1883;
+const char *mqttUser = "";
+const char *mqttPassword = "";
+uint8_t message_char_buffer[MQTT_MAX_PACKET_SIZE];
 
-// We collect each device MAC and RSSI
-typedef struct
-{
-	char address[17]; // 67:f1:d2:04:cd:5d
-	int rssi;
-} BeaconData;
-
-// Scan time must be longer than beacon interval
-int beaconScanTime = 4;
-uint8_t bufferIndex = 0; // Found devices counter
-BeaconData buffer[10];	 // Buffer to store found device data
-// ------------------------------------------------------------------------
-
-void printLocalTime()
-{
-	delay(1000);
-	struct tm timeinfo;
-	if (!getLocalTime(&timeinfo))
-	{
-		Serial.println("Failed to obtain time");
-		return;
-	}
-	Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
-
-/*
- * Construct SenML compatible message with multiple measurements
- * see:
- * https://tools.ietf.org/html/draft-jennings-senml-10
- * Internal temp sensor:
- * https://github.com/pcbreflux/espressif/blob/master/esp32/arduino/sketchbook/ESP32_int_temp_sensor/ESP32_int_temp_sensor.ino
- */
-
-class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
-{
-public:
-	void onResult(BLEAdvertisedDevice advertisedDevice)
-	{
-		extern uint8_t bufferIndex;
-		extern BeaconData buffer[];
-		if (bufferIndex >= 10)
-		{
-			return;
-		}
-		// RSSI
-		if (advertisedDevice.haveRSSI())
-		{
-			buffer[bufferIndex].rssi = advertisedDevice.getRSSI();
-		}
-		else
-		{
-			buffer[bufferIndex].rssi = 0;
-		}
-
-		// MAC is mandatory for BT to work
-		strcpy(buffer[bufferIndex].address, advertisedDevice.getAddress().toString().c_str());
-
-		bufferIndex++;
-		// Print everything via serial port for debugging
-		Serial.printf("MAC: %s \n", advertisedDevice.getAddress().toString().c_str());
-		Serial.printf("name: %s \n", advertisedDevice.getName().c_str());
-		Serial.printf("RSSI: %d \n", advertisedDevice.getRSSI());
-	}
-};
-void ScanBeacons()
-{
-	delay(1000);
-	BLEScan *pBLEScan = BLEDevice::getScan(); // create new scan
-	MyAdvertisedDeviceCallbacks cb;
-	pBLEScan->setAdvertisedDeviceCallbacks(&cb);
-	pBLEScan->setActiveScan(true); // active scan uses more power, but get results faster
-	BLEScanResults foundDevices = pBLEScan->start(beaconScanTime);
-	Serial.print("Devices found: ");
-
-	for (uint8_t i = 0; i < bufferIndex; i++)
-	{
-		Serial.print(buffer[i].address);
-		Serial.print(" : ");
-		Serial.println(buffer[i].rssi);
-	}
-
-	// Stop BLE
-	pBLEScan->stop();
-	delay(1000);
-	Serial.println("Scan done!");
-}
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void connectWiFi()
 {
@@ -114,34 +35,68 @@ void connectWiFi()
 		Serial.println("Connecting to WiFi..");
 	}
 	Serial.println("Connected to the WiFi network");
+
+	Serial.println("");
+	Serial.print("Connected to WiFi network with IP Address: ");
+	Serial.println(WiFi.localIP());
+}
+
+void connectMQTT()
+{
+	client.setServer(mqttServer, mqttPort);
+	Serial.println("Connecting to MQTT...");
+	if (client.connect("ESP32Client", mqttUser, mqttPassword))
+	{
+		Serial.println("connected");
+	}
+	else
+	{
+		Serial.print("failed with state ");
+		Serial.print(client.state());
+		delay(2000);
+	}
 }
 
 void setup()
 {
 	Serial.begin(115200);
-	WiFi.begin(ssid, password);
-
-	while (WiFi.status() != WL_CONNECTED)
-	{
-		delay(1000);
-		Serial.println("Connecting to WiFi...");
-	}
-
-	Serial.println("Connected to WiFi");
-
-	// init and get the time
-	configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-	BLEDevice::init("BLE"); // Can only be called onces
+	connectWiFi();
 }
 
 void loop()
 {
-	ScanBeacons();
+	boolean result;
 	// Reconnect WiFi if not connected
 	while (WiFi.status() != WL_CONNECTED)
 	{
 		connectWiFi();
 	}
 
-	printLocalTime();
+	// Reconnect to MQTT if not connected
+	while (!client.connected())
+	{
+		connectMQTT();
+	}
+	client.loop();
+
+	// SenML begins
+	String payloadString = "{\"e\": esp/123/123 ";
+	payloadString += "\"}";
+
+	// Print and publish payload
+	Serial.print("MAX len: ");
+	Serial.println(MQTT_MAX_PACKET_SIZE);
+
+	Serial.print("Payload length: ");
+	Serial.println(payloadString.length());
+	Serial.println(payloadString);
+
+	payloadString.getBytes(message_char_buffer, payloadString.length() + 1);
+	result = client.publish("/beacons/office", message_char_buffer, payloadString.length(), false);
+	Serial.print("PUB Result: ");
+	Serial.println(result);
+
+	// Start over the scan loop
+	//  Add delay to slow down publishing frequency if needed.
+	delay(1000);
 }
